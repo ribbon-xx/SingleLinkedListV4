228fb7ad18160898da0f587d79716ee73073f137
ribbon-xx <mr.ribbon@gmail.com>
2013-07-23 14:05:00 +0700

    Step 2 RED

--- src/com/qsoft/longdt/SingleLinkedList.java
+++ src/com/qsoft/longdt/SingleLinkedList.java
@@ -4,14 +4,23 @@ import java.util.ArrayList;
 
 public class SingleLinkedList {
 
-	private ArrayList<Object> listObject;
+	private ArrayList<Node> listObject;
 
 	public SingleLinkedList() {
-		listObject = new ArrayList<Object>();
+		listObject = new ArrayList<Node>();
+	}
+
+	public SingleLinkedList(ArrayList<Node> strArray) {
+		listObject = new ArrayList<Node>();
+		listObject.addAll(strArray);
 	}
 
 	public int size() {
 		return listObject.size();
+	}
+
+	public Object getList() {
+		return null;
 	}
 
 }
--- test/com/qsoft/longdt/SingleLinkedListTest.java
+++ test/com/qsoft/longdt/SingleLinkedListTest.java
@@ -1,12 +1,37 @@
 package com.qsoft.longdt;
 
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 
 public class SingleLinkedListTest extends TestCase {
+
+	private ArrayList<Node> strArray;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		strArray = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			Node node = new Node();
+			node.setNode("String " + i);
+			int nextIndex = i < 4 ? i + 1 : -1;
+			int prevIndex = i > 0 ? i - 1 : -1;
+			node.setNextNodeIndex(nextIndex);
+			node.setPrevNodeIndex(prevIndex);
+			strArray.add(node);
+		}
+	}
 
 	// Step 1
 	public void testCreateNewLinkListWithEmptyList() {
 		SingleLinkedList linkedList = new SingleLinkedList();
 		assertEquals(0, linkedList.size());
+	}
+
+	// Step 2
+	public void testCreateNewLinkListWithObjectList() {
+		SingleLinkedList list = new SingleLinkedList(strArray);
+		assertEquals(strArray, list.getList());
 	}
 }
